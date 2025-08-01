0531e85ed1b7eca4487ced3f16768f462a4b00ec
Notoriousbrain <rohitsingh66604@gmail.com>
2025-08-01 23:37:56 +0530

    the landing page

--- apps/docs/next.config.ts
+++ apps/docs/next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  redirects: async () => [
+    {
+      source: "/dashboard/:path*",
+      destination: "/dashboard_app/:path*", // Temp workaround if dashboard uses a different basePath
+      permanent: false,
+    },
+  ],
 };
 
 export default nextConfig;
